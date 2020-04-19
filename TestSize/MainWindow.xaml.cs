e047b6e5f97931cd58b8317253bc67ba187e42e0
Pavel <p97p@ya.ru>
2020-04-20 02:04:38 +0300

    implement

--- TestSize/MainWindow.xaml.cs
+++ TestSize/MainWindow.xaml.cs
@@ -23,6 +23,22 @@ namespace TestSize
         public MainWindow()
         {
             InitializeComponent();
+
+            SizeChanged += (s, e) =>
+            {
+                Text1.Text = string.Join("\n", new[] {
+                    "Window content size:",
+                    $"{Grid1.ActualWidth:0.###} x {Grid1.ActualHeight:0.###}",
+                    "logical pixels",
+                });
+            };
+
+            Loaded += (o, e) =>
+            {
+                var source = PresentationSource.FromVisual(this);
+                var t = source.CompositionTarget.TransformToDevice;
+                Text2.Text = $"System scale:\n{t.M11:0.###} x {t.M22:0.###}";
+            };
         }
-    }
+    };
 }
